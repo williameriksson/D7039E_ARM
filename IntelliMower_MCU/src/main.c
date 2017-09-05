fa0063bab610caa31393c04a88a3e3cb37092931
Sebastian Emmot <sebastian.emmot@gmail.com>
2017-09-05 11:15:13 +0200

    Removed HAL

--- IntelliMower_MCU/src/main.c
+++ IntelliMower_MCU/src/main.c
@@ -13,14 +13,6 @@ static GPIO_InitTypeDef  GPIO_InitStruct;
 
 int main(void)
 {
-  /* STM32F4xx HAL library initialization:
-       - Configure the Flash prefetch, instruction and Data caches
-       - Configure the Systick to generate an interrupt each 1 msec
-       - Set NVIC Group Priority to 4
-       - Global MSP (MCU Support Package) initialization
-     */
-  HAL_Init();
-
   while (1)
   {
 
