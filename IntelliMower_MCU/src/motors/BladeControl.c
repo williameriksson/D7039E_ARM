50d352c3b28f8f8aff71930e3ed78576778128be
Robin Jonsson <jonrob-3@student.ltu.se>
2017-09-21 09:34:58 +0200

    BladeControl created

--- IntelliMower_MCU/src/motors/BladeControl.c
+++ IntelliMower_MCU/src/motors/BladeControl.c
@@ -4,5 +4,24 @@
  *  Created on: 7 sep. 2017
  *      Author: A1201-admin
  */
+#include "BladeControl.h"
 
+void InitBladeControl () {
+
+	GpioEnable(GPIOA);
+	GpioSetOutput(GPIOA, 8);
+
+}
+
+void StartBlades () {
+
+	GpioSetPinHigh(GPIOA, 8);
+
+}
+
+void StopBlades () {
+
+	GpioSetPinLow(GPIOA, 8);
+
+}
 
