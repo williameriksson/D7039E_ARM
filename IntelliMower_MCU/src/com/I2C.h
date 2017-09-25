a2445545c0a84f1022fda0c65cc3a122bd453ea4
William Eriksson <willeeh@hotmail.com>
2017-09-25 10:51:36 +0200

    Fixed robins mistake

--- IntelliMower_MCU/src/com/I2C.h
+++ IntelliMower_MCU/src/com/I2C.h
@@ -13,9 +13,34 @@
 #include "stm32f4xx_nucleo.h"
 #include "utils/GPIO.h"
 
+#define WHO_AM_I 0x0F
+#define SLAVEADDR					0b1101010
+#define CTRL9_XL_ADDR 				0x18
+#define CTRL1_XL_ADDR 				0x10
+#define INT1_CTRL_ADDR 				0x0D
+#define STATUS_REG_ADDR 			0x1E
+#define OUTX_L_XL 					0x28
+#define OUTX_H_XL 					0x29
+
+
+typedef enum {
+	START,
+	SAK_REG_ADDR,
+	REPEATED_START,
+	RECEIVING,
+	READ_LAST_BYTE,
+	READY_TO_WRITE,
+	STOP
+} I2C_STATE;
+
+typedef enum {
+	TRANSMITTER,
+	RECEIVER
+} I2C_MASTER_ROLE;
+
 void InitI2C();
-int I2CRead();
-int I2CWrite();
+void I2cReadByte(uint8_t, uint8_t, uint8_t*);
+void I2cWriteByte(uint8_t, uint8_t, uint8_t);
 
 
 
