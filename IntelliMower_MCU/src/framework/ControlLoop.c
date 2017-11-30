7b92f3187920e0ac1df6ceeb1e212dab3cc19912
Sebastian Emmot <sebastian.emmot@gmail.com>
2017-11-30 11:02:16 +0100

    Controlloop move to TIM10, angle or dist regulate

--- IntelliMower_MCU/src/framework/ControlLoop.c
+++ IntelliMower_MCU/src/framework/ControlLoop.c
@@ -17,11 +17,12 @@ Point targetPos;
 Point currentPos;
 Line line;
 
+/* For STM studio viewer  */
 double distFromLineTEST;
 double outputPID;
 
 int allowedRange;
-int minAdjust;
+int minAdjust; //for capping controller
 int maxAdjust;
 int disSpeed;
 
@@ -46,14 +47,13 @@ void InitControlLoop(Point *startPoint, Point *targetPoint, int speed) {
 	int loopTime = 10;
 //	Init a PID controller with parameters, ref value should
 //	be distance to the line it should hold (typically 0).
-	InitController(&lineFollowController, 0, 3.0f, 0.0f, 0.0f, loopTime); //dist from line regulator settings
+	InitController(&lineFollowController, 0, 0.5f, 0.0f, 0.1f, loopTime); //dist from line regulator settings
 //	InitController(&lineFollowController, 0, 0.5f, 0.0, 1.0, loopTime);
 
 //	Set up an interrupt to that runs at x hertz, this is the control loop time.
-	InitTimerInterrupt(TIM9, loopTime, 20);
-
+	InitTimerInterrupt(TIM10, loopTime, 20);
 }
-double prevGyroOffset;
+double prevAngleOffset;
 double weightedAngle;
 //ran by the interrupt handler
 void LoopController() {
@@ -61,20 +61,22 @@ void LoopController() {
 	//	Check distance from line (use line eq's),
 	//	feed this into the PID
 	double distanceFromLine = GetDistancePointToLine(&line, &currentPos);
-	distFromLineTEST = distanceFromLine;
+	distFromLineTEST = distanceFromLine; //for value viewer
 
-	double gyroOffset = gyro.yaw;
+//	double angleOffset = gyro.yaw; //Angle from gyro
+	double angleOffset = GetPosAngle(); //Angle from encoders
+
 	float output = RunController(&lineFollowController, distanceFromLine, 0); //regulate on dist from line.
-//	float output = RunController(&lineFollowController, gyroOffset, 0); //on gyro
+//	float output = RunController(&lineFollowController, angleOffset, 0); //regulate on gyro
 
-	output = output;
-	outputPID = output;
+	outputPID = output; //for STM studio monitoring
 
-	weightedAngle = weightedAngle - (gyroOffset - prevGyroOffset);
+	weightedAngle = weightedAngle - (angleOffset - prevAngleOffset);
 	weightedAngle = weightedAngle * 0.99 + posAngle * 0.01;
-	prevGyroOffset = gyroOffset;
+	prevAngleOffset = angleOffset;
+//	output += (weightedAngle * 0.25); //feedforward
+//	output += (angleOffset * 0.25);
 
-	output += (weightedAngle * 0.25);
 	//	Use the return value from the PID to adjust steering accordingly.
 	int leftAdjust = (int)output;
 	int rightAdjust = -1*(int)output;
@@ -91,16 +93,16 @@ void LoopController() {
 void UpdatePIDValue(Point *newPos) {
 	currentPos.x = newPos->x;
 	currentPos.y = newPos->y;
-//	distFromLineTEST = GetDistancePointToLine(&line, &currentPos);
+//	distFromLineTEST = GetDistancePointToLine(&line, &currentPos); //for stm studio viewer
 }
 
 void StopController() {
-	StopTimerInterrupt(TIM9);
+	StopTimerInterrupt(TIM10);
 }
 
 //currently assigned by ControlLoop.c as internal interrupt for PID
-void TIM1_BRK_TIM9_IRQHandler() {
+void TIM1_UP_TIM10_IRQHandler() {
 	LoopController();
-	TIM9->SR &= ~TIM_SR_UIF;
+	TIM10->SR &= ~TIM_SR_UIF;
 }
 
